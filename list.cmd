00da53af8fd914703a8b4c199f2b1467a7c6ec39
vjblind <vjblind@users.noreply.github.com>
2020-10-27 12:07:02 +0100

    windows folder tempalte for everyday project
    this file creat a windows folder tempalte for everyday project

---
+++ list.cmd
@@ -0,0 +1,24 @@
+md  day%DATE:/=_%\Script  day%DATE:/=_%\Documentation  day%DATE:/=_%\ref day%DATE:/=_%\render  day%DATE:/=_%\Documentation day%DATE:/=_%\Assets         day%DATE:/=_%\Assets\terrain day%DATE:/=_%\Assets\Music   day%DATE:/=_%\Assets\Blender
+touch day%DATE:/=_%\README.md
+
+
+echo   #Project >> day%DATE:/=_%\README.md
+echo  #__idea__: >>day%DATE:/=_%\README.md
+echo  #__purpose and why this project__: >>day%DATE:/=_%\README.md
+echo  #__References__: >>day%DATE:/=_%\README.md
+echo  #Planning: >>day%DATE:/=_%\README.md
+echo  #Blockout: >>day%DATE:/=_%\README.md
+echo  #Layout: >>day%DATE:/=_%\README.md
+echo  #HeroShot: >>day%DATE:/=_%\README.md
+echo   #Show Many angle: >>day%DATE:/=_%\README.md
+
+echo   Sky: >>day%DATE:/=_%\README.md
+echo   Lighting: >>day%DATE:/=_%\README.md
+echo   Practical_Light: >>day%DATE:/=_%\README.md
+echo   support_Light: >>day%DATE:/=_%\README.md
+
+echo   Finalise aset: >>day%DATE:/=_%\README.md
+
+echo   postprocess : >>day%DATE:/=_%\README.md
+cd day%DATE:/=_%\ 
+git init
